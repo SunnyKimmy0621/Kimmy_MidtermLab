e3c546d9ec50b9b6b650b191c3e28a04008c7744
Kim Padrones <kimmyonceuuyt@gmail.com>
2025-10-08 23:42:05 +0800

    refactored admin.py

--- KimmyMidtermLab/TheLab/admin.py
+++ KimmyMidtermLab/TheLab/admin.py
@@ -4,6 +4,19 @@ from .models import College, Program, Organization, Student, OrgMember
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname","firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
+
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "organization", "get_member_program", "date_joined",)
+    search_fields = ("student__lastname", "student__firstname",)
+
+    def get_member_program(self, obj):
+        # Access the student's program directly
+        return obj.student.program if obj.student else None
+    get_member_program.short_description = "Program"
 
