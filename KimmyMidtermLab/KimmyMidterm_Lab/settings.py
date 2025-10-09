6c62e88da69dba574f7f80d01931b34df8a2b15c
Kim Padrones <kimmyonceuuyt@gmail.com>
2025-10-09 21:55:08 +0800

    Fixed some Stubborn Issues that i made

--- KimmyMidtermLab/KimmyMidterm_Lab/settings.py
+++ KimmyMidtermLab/KimmyMidterm_Lab/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=4g5d9c3__41verhk-hx&a2yafzsyu_05hy*%rj9=fyiqoll-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['sunnykimmy0621.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
