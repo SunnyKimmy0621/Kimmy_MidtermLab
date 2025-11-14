ca7f1f04b89e5b51a16c9174f4e8e8484930b59c
Nestor Jann Asag <nestorjannasag@gmail.com>
2025-11-14 11:11:52 +0800

    backported fixes from main to kimmy test branch

--- KimmyMidtermLab/KimmyMidterm_Lab/urls.py
+++ KimmyMidtermLab/KimmyMidterm_Lab/urls.py
@@ -1,8 +1,45 @@
-from django.urls import path
-from TheLab.views import HomePageView, LoginPageView
+from django.contrib import admin
+from django.urls import path, include
+from TheLab.views import (
+    HomePageView,
+    CategoryList, NoteList, PriorityList, SubtaskList, TaskList,
+    CategoryCreateView, NoteCreateView, PriorityCreateView, SubtaskCreateView, TaskCreateView,
+    CategoryUpdateView, NoteUpdateView, PriorityUpdateView, SubtaskUpdateView, TaskUpdateView,
+    CategoryDeleteView, NoteDeleteView, PriorityDeleteView, SubtaskDeleteView, TaskDeleteView
+)
+from TheLab import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='dashboard'),
-    path('login/', LoginPageView.as_view(), name='login'),
-]
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('', include('pwa.urls')),
 
+    # LIST VIEWS
+    path('category_list/', CategoryList.as_view(), name='category-list'),
+    path('note_list/', NoteList.as_view(), name='note-list'),
+    path('priority_list/', PriorityList.as_view(), name='priority-list'),
+    path('subtask_list/', SubtaskList.as_view(), name='subtask-list'),
+    path('task_list/', TaskList.as_view(), name='task-list'),
+
+    # CREATE VIEWS
+    path('category_list/add/', CategoryCreateView.as_view(), name='category-add'),
+    path('note_list/add/', NoteCreateView.as_view(), name='note-add'),
+    path('priority_list/add/', PriorityCreateView.as_view(), name='priority-add'),
+    path('subtask_list/add/', SubtaskCreateView.as_view(), name='subtask-add'),
+    path('task_list/add/', TaskCreateView.as_view(), name='task-add'),
+
+    # UPDATE VIEWS
+    path('category_list/<pk>/edit/', CategoryUpdateView.as_view(), name='category-update'),
+    path('note_list/<pk>/edit/', NoteUpdateView.as_view(), name='note-update'),
+    path('priority_list/<pk>/edit/', PriorityUpdateView.as_view(), name='priority-update'),
+    path('subtask_list/<pk>/edit/', SubtaskUpdateView.as_view(), name='subtask-update'),
+    path('task_list/<pk>/edit/', TaskUpdateView.as_view(), name='task-update'),
+
+    # DELETE VIEWS
+    path('category_list/<pk>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    path('note_list/<pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
+    path('priority_list/<pk>/delete/', PriorityDeleteView.as_view(), name='priority-delete'),
+    path('subtask_list/<pk>/delete/', SubtaskDeleteView.as_view(), name='subtask-delete'),
+    path('task_list/<pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+]
--- KimmyMidtermLab/TheLab/urls.py
+++ KimmyMidtermLab/TheLab/urls.py
@@ -1,37 +1,14 @@
 from django.urls import path
-from .views import (
-    HomePageView,
-    LoginPageView,
-    CategoryList, NoteList, PriorityList, SubtaskList, TaskList,
-    CategoryCreateView, NoteCreateView, PriorityCreateView, SubtaskCreateView, TaskCreateView,
-    CategoryUpdateView, NoteUpdateView, PriorityUpdateView, SubtaskUpdateView, TaskUpdateView,
-    CategoryDeleteView, NoteDeleteView, PriorityDeleteView, SubtaskDeleteView, TaskDeleteView
-)
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='dashboard'),
-    path('login/', LoginPageView.as_view(), name='login'),
-    path('categories/', CategoryList.as_view(), name='category'),
-    path('notes/', NoteList.as_view(), name='note-list'),
-    path('priorities/', PriorityList.as_view(), name='priority-list'),
-    path('subtasks/', SubtaskList.as_view(), name='subtask-list'),
-    path('tasks/', TaskList.as_view(), name='task-list'),
-
-    path('categories/add/', CategoryCreateView.as_view(), name='category-add'),
-    path('notes/add/', NoteCreateView.as_view(), name='note-add'),
-    path('priorities/add/', PriorityCreateView.as_view(), name='priority-add'),
-    path('subtasks/add/', SubtaskCreateView.as_view(), name='subtask-add'),
-    path('tasks/add/', TaskCreateView.as_view(), name='task-add'),
-
-    path('categories/<int:pk>/edit/', CategoryUpdateView.as_view(), name='category-edit'),
-    path('notes/<int:pk>/edit/', NoteUpdateView.as_view(), name='note-edit'),
-    path('priorities/<int:pk>/edit/', PriorityUpdateView.as_view(), name='priority-edit'),
-    path('subtasks/<int:pk>/edit/', SubtaskUpdateView.as_view(), name='subtask-edit'),
-    path('tasks/<int:pk>/edit/', TaskUpdateView.as_view(), name='task-edit'),
-
-    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
-    path('notes/<int:pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
-    path('priorities/<int:pk>/delete/', PriorityDeleteView.as_view(), name='priority-delete'),
-    path('subtasks/<int:pk>/delete/', SubtaskDeleteView.as_view(), name='subtask-delete'),
-    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+    # path('', views.HomePageView.as_view, name='dashboard'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('tasks/<int:pk>/', views.task_detail, name='task_detail'),
+    path('notes/', views.note_list, name='note_list'),
+    path('subtasks/', views.subtask_list, name='subtask_list'),
+    path('categories/', views.category_list, name='category_list'),
+    path('priorities/', views.priority_list, name='priority_list'),
 ]
+
+
